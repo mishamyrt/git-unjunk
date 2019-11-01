6a8d18ebcb06554d9606b55409dcce7031d86b84
mishamyrt <misha@myrt.co>
2019-11-02 01:01:40 +0300

    Added test

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
+.PHONY: test
 UNAME := $(shell uname)
+
+test:
+	./test.sh
 
 install:
 	make install_$(UNAME)
 
 install_Darwin:
-	cp ./git-unjunk.sh /usr/local/bin/git-unjunk
+	cp ./bin/git-unjunk /usr/local/bin/git-unjunk
 	chmod +x /usr/local/bin/git-unjunk
 
 install_Linux:
-	cp ./git-unjunk.sh /usr/bin/git-unjunk
+	cp ./bin/git-unjunk /usr/bin/git-unjunk
 	chmod +x /usr/bin/git-unjunk
---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+REPO=".test"
+FILE="test.txt"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' 
+
+mkdir -p $REPO
+cd $REPO
+touch test.txt
+git init
+git add $FILE
+git commit -m "Commit message"
+for i in {1..5}
+do
+   git checkout -b branch-$i
+done
+git checkout master
+../bin/git-unjunk -y
+COUNT=$(git branch | wc -l | xargs)
+if (( $COUNT > 1 )); then
+    printf "${RED}Failed${NC}\n"
+else
+    printf "${GREEN}Passed${NC}\n"
+fi
